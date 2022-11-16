2d60e7eb128e68b22d43d333cd25f79d30080357
Spiros <spiroszoupas@hotmail.com>
2022-11-16 21:05:04 +0000

    added button functionality and POC in order to be able to generate unit tests

--- DrawingAppASE/MainForm.cs
+++ DrawingAppASE/MainForm.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DrawingAppASE
 {
     public partial class MainForm : Form
     {
+        readonly Bitmap OutputBitmap;
+
         public MainForm()
         {
             InitializeComponent();
+            OutputBitmap = new Bitmap(506, 396);
         }
 
         private void MultiLineButton_Click(object sender, EventArgs e)
@@ -24,22 +28,65 @@ namespace DrawingAppASE
 
         private void SingleLineButton_Click(object sender, EventArgs e)
         {
+            string commandTyped = SingleLineBox.Text.Trim().ToLower();
+            Pen pen = new Pen(Color.Red);
+            Graphics graphics = Graphics.FromImage(OutputBitmap);
+            switch (commandTyped)
+            {
+                case "line":
+                    graphics.DrawLine(pen, 0, 0, 100, 100);
+                    break;
+                case "square":
+                    graphics.DrawRectangle(pen, 0, 0, 100, 100);
+                    break;
+                case "circle":
+                    graphics.DrawEllipse(pen, 0, 0, 100, 100);
+                    break;
+            }
 
+            SingleLineBox.Clear();
+            Refresh();
         }
 
         private void MultiLineBox_KeyDown(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Enter)
+            {
+                Console.WriteLine("MultiLineKey pressed");
+            }
         }
 
         private void SingleLineBox_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Console.WriteLine("SingleLineKey pressed");
+                string commandTyped = SingleLineBox.Text.Trim().ToLower();
+                Pen pen = new Pen(Color.Red);
+                Graphics graphics = Graphics.FromImage(OutputBitmap);
+                switch (commandTyped)
+                {
+                    case "line":
+                        graphics.DrawLine(pen, 0, 0, 100, 100);
+                        break;
+                    case "square":
+                        graphics.DrawRectangle(pen, 0, 0, 100, 100);
+                        break;
+                    case "circle":
+                        graphics.DrawEllipse(pen, 0, 0, 100, 100);
+                        break;                 
+                }
 
+                SingleLineBox.Clear();
+                Refresh();
+            }
         }
 
         private void paintBox_Paint(object sender, PaintEventArgs e)
         {
-
+            Graphics graphics = e.Graphics;
+            graphics.DrawImageUnscaled(OutputBitmap, 0, 0);
+            
         }
     }
 }
