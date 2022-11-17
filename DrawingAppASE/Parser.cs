5640ea7ef20afeb823a1f529aac0e9d331c8faf6
Spiros <spiroszoupas@hotmail.com>
2022-11-17 14:30:15 +0000

    created unit tests project

--- DrawingAppASE/Parser.cs
+++ DrawingAppASE/Parser.cs
@@ -8,9 +8,21 @@ namespace DrawingAppASE
 {
     public class Parser
     {
-        public static Action ParseAction(IEnumerable<string>)
+        public static Action ParseAction(IEnumerable<string> commands)
         {
             throw new NotImplementedException();
         }
+
+        public static IEnumerable<int> ParseInt(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Action ParseInput(string input) //should output command
+        {
+            throw new NotImplementedException();
+        }
+
+        //so prob ParseAction parses it then passes maybe the command only to ParseInput as a string and outputs a command object so which command? 
     }
 }
---
+++ DrawingAppASETests/ParserTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DrawingAppASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingAppASE.Tests
+{
+    [TestClass()]
+    public class ParserTests
+    {
+        [TestMethod()]
+        public void ParseAction_oneMatchingStringLowerCase_returnsCorrectAction()
+        {
+            var input = new List<string> { "circle" };
+
+            var result = Parser.ParseAction(input);
+
+            //Assert.AreEqual(Action.Circle, result); what?
+        }
+
+        public void ParseAction_oneMatchingStringUpperCase_returnsCorrectAction()
+        {
+            var input = new List<string> { "CIRCLE" };
+
+            var result = Parser.ParseAction(input);
+
+            //Assert.AreEqual(Action.Circle, result); what?
+        }
+
+        public void ParseAction_oneMatchingStringMixedCase_returnsCorrectAction()
+        {
+            var input = new List<string> { "cIrClE" };
+
+            var result = Parser.ParseAction(input);
+
+            //Assert.AreEqual(Action.Circle, result); what?
+        }
+
+        public void ParseAction_oneMatchingStringTwoNonMatching_returnsCorrectAction()
+        {
+            var input = new List<string> { "circle", "pear", "banana" };
+
+            var result = Parser.ParseAction(input);
+
+            //Assert.AreEqual(Action.Circle, result); what?
+        }
+
+        public void ParseAction_twoMatchingStrings_returnsFirstAction()
+        {
+            var input = new List<string> { "circle" };
+
+            var result = Parser.ParseAction(input);
+
+            //Assert.AreEqual(Action.Circle, result); what?
+        }
+    }
+}
